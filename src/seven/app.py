1755126e74e98ed3d30781c60b58083aa4b1dd88
José Quintela <jose.quintela@edge.ufal.br>
2025-04-03 10:41:04 -0300

    Primeiro commit

---
+++ src/seven/app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get('/', response_class=HTMLResponse)
+def read_root():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+        <p> Bom dia calabreso</p>
+      </body>
+    </html>"""
