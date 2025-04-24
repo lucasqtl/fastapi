5700e8082c4371375101890ac4d6c2a1973227f1
lucasqtl <lucasquintela04@gmail.com>
2025-04-24 12:14:19 -0300

    Implementando rotas CRUD

--- src/seven/app.py
+++ src/seven/app.py
@@ -1,18 +1,51 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+
+from seven.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/', response_class=HTMLResponse)
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return """
-    <html>
-      <head>
-        <title> Nosso olá mundo!</title>
-      </head>
-      <body>
-        <h1> Olá Mundo </h1>
-        <p> Bom dia calabreso</p>
-      </body>
-    </html>"""
+    return {'message': 'lucas Mundo'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1: 
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        ) 
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id 
+
+    return user_with_id
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
